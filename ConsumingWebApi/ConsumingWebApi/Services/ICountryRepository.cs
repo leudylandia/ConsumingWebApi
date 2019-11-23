c28483ea44ad42e4f9c19baaad7baf411b136527
Leudy David <leudylandia26@hotmail.com>
2019-11-23 14:03:12 -0400

    Initial Project, Country and Author Interface

---
+++ ConsumingWebApi/ConsumingWebApi/Services/ICountryRepository.cs
@@ -0,0 +1,16 @@
+﻿using ConsumingWebApi.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsumingWebApi.Services
+{
+    public interface ICountryRepository
+    {
+        IEnumerable<CountryDto> GetCountries();
+        CountryDto GetCountryById(int countryId);
+        CountryDto GetCountryOfAnAuthor(int authorId);
+        IEnumerable<AuthorDto> GetAuthorsFromACountry(int countryId);
+    }
+}
